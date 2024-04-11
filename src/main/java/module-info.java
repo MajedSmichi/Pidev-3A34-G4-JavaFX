43f9525c49820f7934c4531c1949f3c046e6011e
koussayx8 <belhouchet.koussay@esprit.tn>
2024-04-11 12:54:07 +0100

    CRUD final with Table View

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.gestionreclamation {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires com.dlsc.formsfx;
+    //requires com.dlsc.formsfx;
     requires java.sql;
     //requires java.persistence;
     opens SportHub to javafx.fxml;
