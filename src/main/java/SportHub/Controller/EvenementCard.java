51c4624e39f5b78734fddbed69e2177fb844016f
koussayx8 <belhouchet.koussay@esprit.tn>
2024-04-15 02:57:26 +0100

    Card Evenement back

---
+++ src/main/java/SportHub/Controller/EvenementCard.java
@@ -0,0 +1,49 @@
+package SportHub.Controller;
+
+import SportHub.Entity.Evenement;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class EvenementCard {
+
+    @FXML
+    private Label Date;
+
+    @FXML
+    private Pane PaneEvenement;
+
+    @FXML
+    private Label name;
+
+    @FXML
+    private ImageView imageEvnement;
+
+    @FXML
+    private Button supprimer;
+
+    private Evenement event;
+
+
+public void setData(Evenement event) {
+    this.event = event;
+    Date.setText(String.valueOf(event.getDateEvenement())); // Assuming Date is a Label
+    name.setText(event.getNom()); // Assuming FullName is a Label
+
+
+    // Set the image
+    String imageUrl = event.getImageEvenement();
+    if (imageUrl != null && !imageUrl.isEmpty()) {
+        Image image = new Image(imageUrl);
+        imageEvnement.setImage(image);
+    } else {
+        // Handle the case when imageUrl is null or empty
+        // For example, set a default image
+    }
+}
+}
