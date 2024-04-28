b267cc63049f340e711529d4fb0bcbe6768e5227
koussayx8 <belhouchet.koussay@esprit.tn>
2024-04-28 03:20:20 +0100

    TRI evenement

--- src/main/java/SportHub/Services/TwilioService.java
+++ src/main/java/SportHub/Services/TwilioService.java
@@ -7,9 +7,8 @@ import com.twilio.type.PhoneNumber;
 public class TwilioService {
 
     // These are your Twilio Account SID and Auth Token
-    private static final String ACCOUNT_SID = "vv";
-    private static final String AUTH_TOKEN = "vv";
-
+private static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+private static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
     public void sendSms(String to, String from, String body) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
